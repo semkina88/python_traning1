68d68a6a79b000c51bc69265819c2a0f5121e595
olga.semkina <semkina88@yandex.ru>
2022-08-21 17:50:22 +0300

    рефакторинг тестового сценария по созданию группы

--- Test_Add_Group.py
+++ Test_Add_Group.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
+
+
 from selenium import webdriver
-# Указываем полный путь к geckodriver.exe на вашем ПК.
-# driver = webdriver.Firefox()
-# driver.get('http://localhost/addressbook/group.php')
 
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -18,45 +14,83 @@ class TestAddGroup(unittest.TestCase):
         # self.wd.get('http://localhost/addressbook/group.php')
         self.wd.implicitly_wait(30)
 
-    
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        # wd.find_element_by_xpath("//input[@value='Login']").click()
-        # wd.find_element_by_xpath("//input[@value='Login']").click()
-        # wd.find_element_by_name("user").click()
-        wd.find_element("name","user").click()
-        wd.find_element("name","user").clear()
-        wd.find_element("name","user").send_keys("admin")
-        wd.find_element("name","pass").clear()
-        wd.find_element("name","pass").send_keys("secret")
-        wd.find_element("xpath","//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_page(wd)
+        self.fill_group_creation(wd, name="name1", header="onn", footer="dfg")
+        self.submit_group_creation(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # logout
+        wd.find_element("link text", "Logout").click()
+
+    def return_to_groups_page(self, wd):
+        # return to groups page
+        wd.find_element("link text", "group page").click()
+
+    def submit_group_creation(self, wd):
+        # submit group creation
+        wd.find_element("xpath", "//input[@value='Enter information']").click()
+
+    def fill_group_creation(self, wd, name, header, footer):
+        # submit group creation
+        wd.find_element("xpath", "//input[@value='New group']").click()
+        # fill group form
+        wd.find_element("name", "group_name").click()
+        wd.find_element("name", "group_name").clear()
+        wd.find_element("name", "group_name").send_keys(name)
+        wd.find_element("name", "group_header").click()
+        wd.find_element("name", "group_header").clear()
+        wd.find_element("name", "group_header").send_keys(header)
+        wd.find_element("name", "group_footer").click()
+        wd.find_element("name", "group_footer").clear()
+        wd.find_element("name", "group_footer").send_keys(footer)
+
+    def open_group_page(self, wd):
+        # open group page
         wd.find_element("link text", "groups").click()
-        wd.find_element("xpath","//input[@value='New group']").click()
-        wd.find_element("name","group_name").click()
-        wd.find_element("name","group_name").clear()
-        wd.find_element("name","group_name").send_keys("name1")
-        wd.find_element("name","group_header").click()
-        wd.find_element("name","group_header").clear()
-        wd.find_element("name","group_header").send_keys("onn")
-        wd.find_element("name","group_footer").click()
-        wd.find_element("name","group_footer").clear()
-        wd.find_element("name","group_footer").send_keys("ijbjk")
-        wd.find_element("xpath","//input[@value='Enter information']").click()
-        wd.find_element("link text","group page").click()
-        wd.find_element("link text","Logout").click()
-    
+
+    def login(self, wd, username, password):
+        # login
+        wd.find_element("name", "user").click()
+        wd.find_element("name", "user").clear()
+        wd.find_element("name", "user").send_keys(username)
+        wd.find_element("name", "pass").clear()
+        wd.find_element("name", "pass").send_keys(password)
+        wd.find_element("xpath", "//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        # open home page
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
-   
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_page(wd)
+        self.fill_group_creation(wd, name="", header="", footer="")
+        self.submit_group_creation(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
