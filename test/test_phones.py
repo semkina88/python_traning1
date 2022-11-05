3745fa208cb5c47e27a6cfe4aa0b24b121b87792
semkina88 <semkina88@yandex.ru>
2022-11-05 15:53:15 +0300

    Правка теста по заданию 14

--- test/test_phones.py
+++ test/test_phones.py
@@ -17,17 +17,25 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.work_phone == contact_from_edit_page.work_phone
     assert contact_from_view_page.secondary_phone == contact_from_edit_page.secondary_phone
 
+
 def clear(s):
     return re.sub("[ ()-]", "", s)
+
+
+def clear_emails(s):
+    return re.sub(" + ", " ", s)
+
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                    [contact.home_phone, contact.cell_phone, contact.work_phone, contact.secondary_phone]))))
+                                       [contact.home_phone, contact.cell_phone, contact.work_phone,
+                                        contact.secondary_phone]))))
+
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != '',
-                            map(lambda x: clear(x),
+                            map(lambda x: clear_emails(x),
                                 filter(lambda x: x is not None,
                                        [contact.email, contact.email2, contact.email3]))))
