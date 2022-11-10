020b515c3a183d62324d0386c730bf016207e1dd
semkina88 <semkina88@yandex.ru>
2022-11-10 12:26:02 +0300

    Задание 17

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 
 import pytest
 
@@ -14,9 +15,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            tagret = json.load(config_file)
-    base_url = request.config.getoption("--baseUrl")
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            tagret = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=tagret['baseUrl'])
     fixture.session.ensure_login(username=tagret['username'], password=tagret['password'])
