53f984a986b608212d494b35b24bfe07a9465ca7
semkina88 <semkina88@yandex.ru>
2022-12-05 13:26:52 +0300

    правка теста по модификации контакта 20 задание

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,4 +1,5 @@
 import random
+from random import randrange
 
 from model.class_for_test import Contact
 
@@ -7,13 +8,15 @@ def test_modify_contact_firstname(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="bla"))
     old_contacts = db.get_contact_list()
-    # index = randrange(len(old_contacts))
-    contact = Contact(firstname="test contact")
+    index = randrange(len(old_contacts))
+    contact = Contact(firstname="test")
     selection = random.choice(old_contacts)
-    # contact.id = old_contacts[index].id
+    contact.id = old_contacts[index].id
     app.contact.modify_contact_by_id(selection.id, contact)
     new_contacts = db.get_contact_list()
-    # assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-    assert len(old_contacts) == len(new_contacts)
+    old_contacts[index] = contact
+    old_contacts[index].id = new_contacts[index].id
+    old_contacts[index].firstname = new_contacts[index].firstname
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
