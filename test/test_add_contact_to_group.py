aa4b8c8dbcc00be91f51551a21726282c82bfbd4
semkina88 <semkina88@yandex.ru>
2022-12-14 11:22:00 +0300

    исправление тестов по модификации контактов и добавлении контакта в группу

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -8,12 +8,15 @@ def test_add_contact_to_group(app, orm):
         app.contact.create(Contact(firstname="Test contact"))
     if len(orm.get_group_list()) == 0:
         app.group.create(Group(name="test", header="group test", footer="test group"))
-    old_contacts = orm.get_contact_list()
-    contact = random.choice(old_contacts)
     old_groups = orm.get_group_list()
     group = random.choice(old_groups)
+    contacts_not_in_group = orm.get_contacts_not_in_group(Group(id=group.id))
+    if len(contacts_not_in_group) == 0:
+        app.contact.create(Contact(firstname="Contact1"))
+    contacts_not_in_group = orm.get_contacts_not_in_group(Group(id=group.id))
+    contact = random.choice(contacts_not_in_group)
     app.contact.add_contact_to_group(contact.id, group.id)
-    contacts_in_group = orm.get_contacts_in_group(group)
-    assert contact in contacts_in_group
+    contacts_in_group_db_new = orm.get_contacts_in_group(group)
+    assert contact in contacts_in_group_db_new
 
-# c965c306911b47baa8bbd954a41d55e1
+
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,26 +4,17 @@ from model.class_for_test import Contact
 
 
 def test_modify_contact_firstname(app, db, check_ui):
-    # если контактов нет создадим контакт
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="bla"))
-    # получаем текущий список контактов из бд
     old_contacts = db.get_contact_list()
-    # Создадим сущность контакт с определенным именем
     contact = Contact(firstname="swyug")
-    # выбиарем слуйное число по длине списка контактов
     index = randrange(len(old_contacts))
-    # Присвоим созданной сущности id выбранного контакт из списка
     contact.id = old_contacts[index].id
-    # Через интерфейс изменим имя для выбранного контакта
     app.contact.modify_contact_by_id(contact.id, contact)
-    # Получим актуальный список контактов из бд
     new_contacts = db.get_contact_list()
-    # Проведем сравнение имен для имеющейся сущности и контакта из актуального списка с известным id
     contact.firstname == new_contacts[index].firstname
-    # Установим флаг для проведения проверки
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
-        # проверим состав списков
         assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
