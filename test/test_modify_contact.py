db7871815ba2d6d83cb4cfb820b1a335ea3c12df
semkina88 <111438415+semkina88@users.noreply.github.com>
2022-12-16 09:58:03 +0300

    Update test_modify_contact.py

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -12,7 +12,9 @@ def test_modify_contact_firstname(app, db, check_ui):
     contact.id = old_contacts[index].id
     app.contact.modify_contact_by_id(contact.id, contact)
     new_contacts = db.get_contact_list()
-    contact.firstname == new_contacts[index].firstname
+#     contact.firstname == new_contacts[index].firstname
+    old_contacts[index] = contact
+    assert len(old_contacts) == len(new_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
